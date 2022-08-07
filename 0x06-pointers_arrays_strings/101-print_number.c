c5b57d0cdbf303f88127a397841e3e8bad660cdc
olesso <olessokisia@gmail.com>
2022-08-07 05:52:12 -0700

    well

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,67 +3,67 @@
 
 
 /**
-
- * print_number - prints an integer.
-
- * @n: input integer.
-
- * Return: no return.
-
- */
+ *
+ *  * print_number - prints an integer.
+ *
+ *   * @n: input integer.
+ *
+ *    * Return: no return.
+ *
+ *     */
 
 void print_number(int n)
 
 {
 
-	unsigned int m, d, count;
+		unsigned int m, d, count;
 
 
 
-	if (n < 0)
+			if (n < 0)
 
-	{
+					{
 
-		_putchar(45);
+								_putchar(45);
 
-		m = n * -1;
+										m = n * -1;
 
-	}
+											}
 
-	else
+				else
 
-	{
+						{
 
-		m = n;
+									m = n;
 
-	}
-
-
-
-	d = m;
-
-	count = 1;
+										}
 
 
 
-	while (d > 9)
+					d = m;
 
-	{
-
-		d /= 10;
-
-		count *= 10;
-
-	}
+						count = 1;
 
 
 
-	for (; count >= 1; count /= 10)
+							while (d > 9)
 
-	{
+									{
 
-		_putchar(((m / count) % 10) + 48);
+												d /= 10;
 
-	}
+														count *= 10;
+
+															}
+
+
+
+								for (; count >= 1; count /= 10)
+
+										{
+
+													_putchar(((m / count) % 10) + 48);
+
+														}
 
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,20 +3,20 @@
 #include <stdio.h>
 
 /**
-
- * infinite_add - adds two numbers
-
- * @n1: number one.
-
- * @n2: number two.
-
- * @r: buffer that the function will use to store the result.
-
- * @size_r: buffer size:
-
- * Return: the pointer to dest.
-
- */
+ *
+ *  * infinite_add - adds two numbers
+ *
+ *   * @n1: number one.
+ *
+ *    * @n2: number two.
+ *
+ *     * @r: buffer that the function will use to store the result.
+ *
+ *      * @size_r: buffer size:
+ *
+ *       * Return: the pointer to dest.
+ *
+ *        */
 
 
 
@@ -24,84 +24,85 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 {
 
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+		int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
 
 
-	while (*(n1 + c1) != '\0')
+			while (*(n1 + c1) != '\0')
 
-		c1++;
+						c1++;
 
-	while (*(n2 + c2) != '\0')
+				while (*(n2 + c2) != '\0')
 
-		c2++;
+							c2++;
 
-	if (c1 >= c2)
+					if (c1 >= c2)
 
-		bg = c1;
+								bg = c1;
 
-	else
+						else
 
-		bg = c2;
+									bg = c2;
 
-	if (size_r <= bg + 1)
+							if (size_r <= bg + 1)
 
-		return (0);
+										return (0);
 
-	r[bg + 1] = '\0';
+								r[bg + 1] = '\0';
 
-	c1--, c2--, size_r--;
+									c1--, c2--, size_r--;
 
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+										dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 
-	while (bg >= 0)
+											while (bg >= 0)
 
-	{
+													{
 
-		op = dr1 + dr2 + add;
+																op = dr1 + dr2 + add;
 
-		if (op >= 10)
+																		if (op >= 10)
 
-			add = op / 10;
+																						add = op / 10;
 
-		else
+																				else
 
-			add = 0;
+																								add = 0;
 
-		if (op > 0)
+																						if (op > 0)
 
-		*(r + bg) = (op % 10) + 48;
+																									*(r + bg) = (op % 10) + 48;
 
-		else
+																								else
 
-			*(r + bg) = '0';
+																												*(r + bg) = '0';
 
-		if (c1 > 0)
+																										if (c1 > 0)
 
-			c1--, dr1 = *(n1 + c1) - 48;
+																														c1--, dr1 = *(n1 + c1) - 48;
 
-		else
+																												else
 
-			dr1 = 0;
+																																dr1 = 0;
 
-		if (c2 > 0)
+																														if (c2 > 0)
 
-			c2--, dr2 = *(n2 + c2) - 48;
+																																		c2--, dr2 = *(n2 + c2) - 48;
 
-		else
+																																else
 
-			dr2 = 0;
+																																				dr2 = 0;
 
-		bg--, size_r--;
+																																		bg--, size_r--;
 
-	}
+																																			}
 
-	if (*(r) == '0')
+												if (*(r) == '0')
 
-		return (r + 1);
+															return (r + 1);
 
-	else
+													else
 
-		return (r);
+																return (r);
 
+}
 }
